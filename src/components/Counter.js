ced212064abb174d302e58b51ccfb0b5f8085819
Kourtney Queeley <Kourtney_queeley@hotmail.com>
2023-06-05 22:51:34 +0100

    Counter style

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,14 @@ export class Counter extends Component{
     }
 
     render() {
-        return <div>Counter</div>
-    }
+        return (
+        <div className="border m-3 p-4 rounded">
+            <div className="text-center ">Counter: 0</div>
+            <div className="d-flex justify-content-evenly">
+                <button className="btn btn-success">Decrement</button>
+                <button className="btn btn-danger">Reset</button>
+                <button className="btn btn-success">Increment</button>
+            </div>
+        </div>
+    )}
 }

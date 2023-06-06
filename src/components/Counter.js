20cccb06f546718557da13d618adf718a4b4f2eb
Kourtney Queeley <Kourtney_queeley@hotmail.com>
2023-06-06 17:15:04 +0100

    new chnages

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,16 +3,40 @@ import { Component } from "react"
 export class Counter extends Component{
     constructor(props) {
         super(props);
+
+        this.state = { count: 0 } ;
     }
+    
+
+    handleDecrement = () => {
+        if (this.state.count > 0){
+            this.setState({
+             count: this.state.count -1,
+            })
+        }
+    }
+
+    handleIncrement = () => {
+        this.setState({
+            count: this.state.count + 1,
+        })
+    }
+
+    handleReset = () => {
+        this.setState({
+            count: 0,
+        })
+    }
+
 
     render() {
         return (
         <div className="border m-3 p-4 rounded">
-            <div className="text-center ">Counter: 0</div>
+            <div className="text-center sub-title ">Counter: 0{this.state.count}</div>
             <div className="d-flex justify-content-evenly">
-                <button className="btn btn-success">Decrement</button>
-                <button className="btn btn-danger">Reset</button>
-                <button className="btn btn-success">Increment</button>
+                <button className="btn btn-success" onClick={this.handleDecrement}>Decrement</button>
+                <button className="btn btn-danger" onClick={this.handleReset}>Reset</button>
+                <button className="btn btn-success" onClick={this.handleIncrement}>Increment</button>
             </div>
         </div>
     )}
